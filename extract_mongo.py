d13b070a01765f6e5ae3de99e0812c382a9a462e
Intanhula <intantesting22@gmail.com>
2024-07-17 02:44:59 +0000

    penambahan comment

--- extract_mongo.py
+++ extract_mongo.py
@@ -1,7 +1,7 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession #import library
 
-input_uri = "mongodb://182.23.45.57/Ticket.Ticketlist"
-output_uri = "mongodb://182.23.45.57/Ticket.Ticketlist"
+input_uri = "mongodb://182.23.45.57/Ticket.Ticketlist" # get input uri database and collection
+output_uri = "mongodb://182.23.45.57/Ticket.Ticketlist" # get output uri database and collection
 
 
 myspark = SparkSession \
@@ -10,10 +10,10 @@ myspark = SparkSession \
     .config("spark.mongodb.input.uri", input_uri) \
     .config("spark.mongodb.output.uri", output_uri) \
     .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:2.4.2") \
-    .getOrCreate()
-sqlContext =SparkSession(myspark)
+    .getOrCreate()  #Initiate SparkSession
+sqlContext =SparkSession(myspark) # initiate sqlcontext 
 
-ticket_list_df= myspark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+ticket_list_df= myspark.read.format("com.mongodb.spark.sql.DefaultSource").load() #initiate data frame
 
-print(ticket_list_df.show())
-myspark.sparkContext.setLogLevel("ERROR")
+print(ticket_list_df.show()) #display data frame
+myspark.sparkContext.setLogLevel("ERROR") #display log level error
